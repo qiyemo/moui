ba5883b1157f26d9eab514711f79ba067fc7eb06
qiye.mo <qiye.mo@foxmail.com>
2021-12-01 00:03:15 +0800

    feat - 基础样式

--- docs/components/button.md
+++ docs/components/button.md
@@ -3,7 +3,23 @@
 :::demo 使用`size`、`style`属性来定义 Card 的样式。
 ```vue
 <template>
-  <m-card>卡片</m-card>
+  <m-card>
+    <template #header>
+      <div>Default size card</div>
+    </template>
+    <div>
+      card content
+    </div>
+    <div>
+      card content
+    </div>
+    <div>
+      card content
+    </div>
+    <div>
+      card content
+    </div>
+  </m-card>
 </template>
 ```
 :::
--- packages/components/card/src/card.vue
+++ packages/components/card/src/card.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="m-card">
-  <div class="m-card__header" style="color: red;">
-    <slot name="header">卡片头</slot>
+  <div class="m-card__header font--title3">
+    <slot name="header"></slot>
   </div>
   <div class="m-card__body" :style="bodyStyle">
     <slot></slot>
@@ -17,4 +17,23 @@ export default defineComponent({
   props: cardProps
 });
 </script>
+<style lang="scss">
+.m-card{
+  box-sizing: border-box;
+  border-radius: var(--border-radius-small);
+  box-shadow: var(--border-shadow-light);
+  border-color: var(--color-border-light);
+  border-style: solid;
+  border-width: 1px;
+}
+.m-card .m-card__header, .m-card .m-card__body{
+  padding: var(--spacing-middle);
+}
+.m-card .m-card__header{
+  border-bottom: solid 1px var(--color-border-light);
+}
+.m-card .m-card__body{
+
+}
+</style>
 
