1392ef72c5c233514ed5e5b63f4e5812b29c855e
qiye <coderqiye@163.com>
2022-08-18 13:35:40 +0800

    feat: tag 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import MoUI from './entry';
-
+import MoUI from '../dist/mo-ui.esm.js';
+import '../dist/assets/entry.7b094ac9.css';
 createApp(App).use(MoUI).mount('#app');
